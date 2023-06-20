f5504c4882b618c19e1a52c5c8fa6e287b2918fb
EstherMwiyeria <esthermkanoro@gmail.com>
2023-06-20 10:14:23 +0300

    bills-commit

--- app/src/main/java/com/example/assessments/Login.kt
+++ app/src/main/java/com/example/assessments/Login.kt
@@ -2,10 +2,37 @@ package com.example.assessments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.assessments.databinding.ActivityLoginBinding
+import com.example.assessments.databinding.ActivityMain2Binding
 
 class Login : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-}
+
+
+        override fun onResume() {
+            super.onResume()
+            setContentView(binding.root)
+            binding.btnLogin.setOnClickListener {
+                validateRegistration()
+            }
+        }
+        fun validateRegistration() {
+            val email=binding.etEmail .text.toString()
+            val password =binding.etPassword .text.toString()
+            var error = false
+            if (email.isBlank()) {
+                binding.tilEmail.error = "Name required"
+                error=true
+            }
+            if (password.isBlank()) {
+                binding.tilPassword.error = "Phonenumber required"
+                error=true
+            }
+
+        }
+    }
--- app/src/main/java/com/example/assessments/MainActivity2.kt
+++ app/src/main/java/com/example/assessments/MainActivity2.kt
@@ -9,41 +9,49 @@ import com.example.assessments.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var binding: ActivityMain2Binding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        }
+    override fun onResume() {
+        super.onResume()
+        setContentView(binding.root)
         binding.btnRegister.setOnClickListener {
-
-            val isValid = validateForm()
-
-            if (isValid) {
-
-                val username = binding.etUserName.text.toString()
-                val password = binding.etPass.text.toString()
-
-
-                if (username == "your_username" && password == "your_password") {
-                    startActivity(Intent(this, HomeActivity::class.java))
-                    finish()
-                } else {
-
-                    binding.tvError.text = "Invalid username or password"
-                }
-            }
+            validateRegistration()
         }
     }
+    fun validateRegistration() {
+        val userName = binding.etUserName.text.toString()
+        val phoneNumber =binding.etPhoneNumber .text.toString()
+        val email=binding.etEmail .text.toString()
+        val password =binding.etPassword .text.toString()
+        var error = false
+        if (userName.isBlank()) {
+            binding.tillUserName.error = "Name required"
+            error=true
+        }
+        if (phoneNumber.isBlank()) {
+            binding.tilPhoneNumber.error = "Phonenumber required"
+            error=true
+        }
+        if (email.isBlank()) {
+            binding.tilEmail.error = "Email required"
+            error=true
+        }
 
-    private fun validateForm(): Boolean {
-        val username = binding.etUserName.text.toString()
-        val password = binding.etPassword.text.toString()
-
-
-        return true
+        if (password.isBlank()) {
+            binding.tilPassword.error = "Password required"
+            error=true
+        }
+        if (!error){
+            val intent= Intent(this,Login::class.java)
+            startActivity(intent)
+        }
     }
 }
+
+
 
 
 

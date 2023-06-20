12e85250fbb0e4ce5e78e2cc1bf414a3ff1bc0ba
EstherMwiyeria <esthermkanoro@gmail.com>
2023-06-20 09:06:14 +0300

    bills-commit

--- app/src/main/java/com/example/assessments/adapter.kt
+++ app/src/main/java/com/example/assessments/adapter.kt
@@ -1,50 +1,4 @@
 package com.example.assessments
-//
-//
-//import android.content.Intent
-//import android.view.LayoutInflater
-//import android.view.ViewGroup
-//import androidx.recyclerview.widget.RecyclerView
-//import androidx.recyclerview.widget.RecyclerView.ViewHolder
-//import com.example.assessments.MainActivity2
-//import com.example.assessments.bIllsData
-//import com.example.assessments.databinding.ActivityBillsDataBinding
-//import com.example.contacts.databinding.ContactListItemBinding
-//import com.squareup.picasso.Picasso
-//import jp.wasabeef.picasso.transformations.CropCircleTransformation
-//
-//class adapter(var ContactList:List<bIllsData>) : RecyclerView.Adapter<ContactViewHolder>(){
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-//        val binding = ActivityBillsDataBinding.inflate(LayoutInflater.from(parent.context),parent ,false)
-//        return ContactViewHolder(binding)
-//    }
-//    override fun getItemCount(): Int {
-//        return ContactList.size
-//    }
-//    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
-//        var currentContact =ContactList.get(position)
-//        var  binding=holder.binding
-////        binding.ivAvatar.tag=currentContact.image
-//        binding.tvName.text=currentContact.names
-//        binding.tvphoneNumber.text=currentContact.phoneNumber
-//        binding.tvEmail.text =currentContact.email
-//        binding.tvPassword.text = currentContact.password
-//
-//        binding.tvbtn.setOnClickListener {
-//            val intent = Intent(holder.itemView.context, MainActivity2::class.java)
-//            holder.itemView.context.startActivity(intent)
-//        }
-//        Picasso
-//            .get()
-////            .load(currentContact.image)
-//            .resize(80,80)
-//            .centerCrop()
-//            .transform(CropCircleTransformation())
-////            .into(binding.ivAvatar)
-//    }
-//}
-//class ContactViewHolder( var binding:ActivityBillsDataBinding):ViewHolder(binding.root)
-
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -67,7 +21,7 @@ class BillsAdapter(private val bills: List<Bill>) : RecyclerView.Adapter<BillsAd
         return bills.size
     }
 
-    inner class ViewHolder(private val binding: ItemBillBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: Bill) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(bill: Bill) {
             // Bind the bill data to the views
